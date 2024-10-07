f340095c88156d9d82f8001da83825eb9f684acf
mohamedfakhir <mohamed.fakhir@cloudlink.us>
2024-10-07 17:12:36 +0100

    ddd

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
+import logging
 from flask import Flask, request, send_file
 from flask_cors import CORS
 import io
 from rembg import remove
-import os  # Import the os module
+import os
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS to allow Next.js frontend to communicate with Flask API
+CORS(app)
 
 def remove_background(input_image):
     """Removes the background from an image."""
+    logging.debug("Removing background from image")
     result = remove(input_image)
     return result
 
 @app.route('/remove_background', methods=['POST'])
 def handle_remove_background():
     """Handles the image upload and returns the image without background."""
+    logging.debug("Received request to remove background")
     if 'image' not in request.files:
+        logging.error("No image uploaded")
         return {'error': 'No image uploaded'}, 400
 
     image_file = request.files['image']
@@ -28,4 +34,4 @@ def handle_remove_background():
     return send_file(io.BytesIO(result), mimetype='image/png')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))  # Ensure os is imported
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
